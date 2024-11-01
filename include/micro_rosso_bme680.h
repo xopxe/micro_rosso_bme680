177623e5742184f771e4f4f6f1f5f9ecb0d7a4ca
xopxe <xxopxe@gmail.com>
2024-11-01 11:40:02 -0300

    cleanup

--- include/micro_rosso_bme680.h
+++ include/micro_rosso_bme680.h
@@ -3,16 +3,16 @@
 
 #include <Wire.h>
 
-class EnvBME680 {
+class EnvBME680
+{
 public:
   EnvBME680();
-  static bool setup( TwoWire &wire = Wire,
-                     const char* topic_temp = "/internal/temperature",
-                     const char* topic_humi = "/internal/humidity",
-                     const char* topic_pres = "/internal/pressure",
-                     const char* topic_gasr = "/internal/gas_resistance",
-                     timer_descriptor &timer_report = micro_rosso::timer_report
-  ); 
+  static bool setup(TwoWire &wire = Wire,
+                    const char *topic_temp = "/internal/temperature",
+                    const char *topic_humi = "/internal/humidity",
+                    const char *topic_pres = "/internal/pressure",
+                    const char *topic_gasr = "/internal/gas_resistance",
+                    timer_descriptor &timer_report = micro_rosso::timer_report);
 };
 
-#endif  // __env_bme680_h
+#endif // __env_bme680_h
--- src/micro_rosso_bme680.cpp
+++ src/micro_rosso_bme680.cpp
@@ -21,43 +21,52 @@ static publisher_descriptor pdescriptor_humidity;
 static publisher_descriptor pdescriptor_pressure;
 static publisher_descriptor pdescriptor_gas_resistance;
 
-#define RCCHECK(fn) \
-  { \
-    rcl_ret_t temp_rc = fn; \
-    if ((temp_rc != RCL_RET_OK)) { return false; } \
+#define RCCHECK(fn)              \
+  {                              \
+    rcl_ret_t temp_rc = fn;      \
+    if ((temp_rc != RCL_RET_OK)) \
+    {                            \
+      return false;              \
+    }                            \
   }
-#define RCNOCHECK(fn) \
-  { \
+#define RCNOCHECK(fn)       \
+  {                         \
     rcl_ret_t temp_rc = fn; \
-    (void)temp_rc; \
+    (void)temp_rc;          \
   }
 
 Adafruit_BME680 *bme;
 
 static unsigned long last_read_ms = 0;
 
-EnvBME680::EnvBME680() {
-  msg_temperature.temperature = -273.15;  //0K
+EnvBME680::EnvBME680()
+{
+  msg_temperature.temperature = -273.15; // 0K
   msg_humidity.relative_humidity = 0;
   msg_pressure.fluid_pressure = 0;
   msg_gas_resistance.data = -1;
 }
 
-void report_cb(int64_t last_call_time) {
+void report_cb(int64_t last_call_time)
+{
   int ramaining_millis = bme->remainingReadingMillis();
-  if (ramaining_millis == bme->reading_not_started) {
+  if (ramaining_millis == bme->reading_not_started)
+  {
     bme->beginReading();
     return;
   }
-  if (ramaining_millis > bme->reading_complete) {
+  if (ramaining_millis > bme->reading_complete)
+  {
     return;
   }
-  if (!bme->endReading()) {
+  if (!bme->endReading())
+  {
     return;
   }
 
   unsigned long now = millis();
-  if (now - last_read_ms < READS_INTERVAL_MS) {
+  if (now - last_read_ms < READS_INTERVAL_MS)
+  {
     return;
   }
   last_read_ms = now;
@@ -65,50 +74,57 @@ void report_cb(int64_t last_call_time) {
   float t = bme->temperature;
   float h = bme->humidity;
   float p = bme->pressure / 100.0;
-  float r = bme->gas_resistance / 1000.0;  // KOhm
+  float r = bme->gas_resistance / 1000.0; // KOhm
 
-  if (msg_temperature.temperature != t) {
+  if (msg_temperature.temperature != t)
+  {
     msg_temperature.temperature = t;
     micro_rosso::set_timestamp(msg_temperature.header.stamp);
     RCNOCHECK(rcl_publish(
-      &pdescriptor_temperature.publisher,
-      &msg_temperature,
-      NULL));
+        &pdescriptor_temperature.publisher,
+        &msg_temperature,
+        NULL));
   }
-  if (msg_humidity.relative_humidity != h) {
+  if (msg_humidity.relative_humidity != h)
+  {
     msg_humidity.relative_humidity = h;
     micro_rosso::set_timestamp(msg_humidity.header.stamp);
     RCNOCHECK(rcl_publish(
-      &pdescriptor_humidity.publisher,
-      &msg_humidity,
-      NULL));
+        &pdescriptor_humidity.publisher,
+        &msg_humidity,
+        NULL));
   }
-  if (msg_pressure.fluid_pressure != p) {
+  if (msg_pressure.fluid_pressure != p)
+  {
     msg_pressure.fluid_pressure = p;
     micro_rosso::set_timestamp(msg_pressure.header.stamp);
     RCNOCHECK(rcl_publish(
-      &pdescriptor_pressure.publisher,
-      &msg_pressure,
-      NULL));
+        &pdescriptor_pressure.publisher,
+        &msg_pressure,
+        NULL));
   }
-  if (msg_gas_resistance.data != r) {
+  if (msg_gas_resistance.data != r)
+  {
     msg_gas_resistance.data = r;
     RCNOCHECK(rcl_publish(
-      &pdescriptor_gas_resistance.publisher,
-      &msg_gas_resistance,
-      NULL));
+        &pdescriptor_gas_resistance.publisher,
+        &msg_gas_resistance,
+        NULL));
   }
 }
 
-bool EnvBME680::setup( TwoWire &wire,
-                      const char* topic_temperature,
-                      const char* topic_humiidity,
-                      const char* topic_pressure,
-                      const char* topic_gas_resistance, 
-                      timer_descriptor &timer_report ) {
-  D_println("setup: env_bme680");
+bool EnvBME680::setup(TwoWire &wire,
+                      const char *topic_temperature,
+                      const char *topic_humiidity,
+                      const char *topic_pressure,
+                      const char *topic_gas_resistance,
+                      timer_descriptor &timer_report)
+{
+  D_print("setup: env_bme680... ");
+
   bme = new Adafruit_BME680(&wire);
-  if (!bme->begin()) {
+  if (!bme->begin())
+  {
     return false;
   }
 
@@ -117,33 +133,34 @@ bool EnvBME680::setup( TwoWire &wire,
   bme->setHumidityOversampling(BME680_OS_2X);
   bme->setPressureOversampling(BME680_OS_4X);
   bme->setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme->setGasHeater(320, 150);  // 320*C for 150 ms
+  bme->setGasHeater(320, 150); // 320*C for 150 ms
 
   pdescriptor_temperature.qos = QOS_DEFAULT;
-  pdescriptor_temperature.type_support = (rosidl_message_type_support_t*)
-    ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Temperature);
+  pdescriptor_temperature.type_support = (rosidl_message_type_support_t *)
+      ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Temperature);
   pdescriptor_temperature.topic_name = topic_temperature;
   micro_rosso::publishers.push_back(&pdescriptor_temperature);
 
   pdescriptor_humidity.qos = QOS_DEFAULT;
-  pdescriptor_humidity.type_support = (rosidl_message_type_support_t*)
-    ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, RelativeHumidity);
+  pdescriptor_humidity.type_support = (rosidl_message_type_support_t *)
+      ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, RelativeHumidity);
   pdescriptor_humidity.topic_name = topic_humiidity;
   micro_rosso::publishers.push_back(&pdescriptor_humidity);
 
   pdescriptor_pressure.qos = QOS_DEFAULT;
-  pdescriptor_pressure.type_support = (rosidl_message_type_support_t*)
-    ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, FluidPressure);
+  pdescriptor_pressure.type_support = (rosidl_message_type_support_t *)
+      ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, FluidPressure);
   pdescriptor_pressure.topic_name = topic_pressure;
   micro_rosso::publishers.push_back(&pdescriptor_pressure);
 
   pdescriptor_gas_resistance.qos = QOS_DEFAULT;
-  pdescriptor_gas_resistance.type_support = (rosidl_message_type_support_t*)
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32);
+  pdescriptor_gas_resistance.type_support = (rosidl_message_type_support_t *)
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32);
   pdescriptor_gas_resistance.topic_name = topic_gas_resistance;
   micro_rosso::publishers.push_back(&pdescriptor_gas_resistance);
 
   timer_report.callbacks.push_back(&report_cb);
 
+  D_println("done.");
   return true;
 }
